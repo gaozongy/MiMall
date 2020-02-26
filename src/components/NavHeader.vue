427f86ffb06336686a2e0bf7e7682d0213045e71
GaozyDev <670453367@qq.com>
2020-02-26 14:29:19 +0800

    顶部导航栏组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -28,7 +28,19 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product" v-for="(item, index) in phoneList" :key="index">
+                  <a v-bind:href="'/#/product/' + item.id" traget="_blank">
+                    <div class="pro-img">
+                      <img :src="item.mainImage" :alt="item.subtitle" />
+                    </div>
+                    <div class="pro-name">{{item.name}}</div>
+                    <div class="pro-price">{{item.price | currency}}</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
             <span>Redmi红米</span>
@@ -52,12 +64,50 @@
 
 <script>
 export default {
-  name: "nav-header"
+  name: "nav-header",
+  data() {
+    return {
+      username: "",
+      phoneList: []
+    };
+  },
+  filters: {
+    currency(val) {
+      if (!val) return "￥0.00";
+      return "￥" + val.toFixed(2) + "元";
+    }
+  },
+  mounted() {
+    this.getProductList();
+  },
+  methods: {
+    login() {
+      this.$router.push("/login");
+    },
+    getProductList() {
+      this.axios
+        .get("/products", {
+          params: {
+            categoryId: "100012",
+            pageSize: 6
+          }
+        })
+        .then(res => {
+          if (res.list.length >= 6) {
+            this.phoneList = res.list.slice(0, 6);
+          }
+        });
+    },
+    goToCart() {
+      this.$router.push("/cart");
+    }
+  }
 };
 </script>
 <style lang="scss">
 @import "./../assets/scss/base.scss";
 @import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
 .header {
   .nav-topbar {
     height: 39px;
@@ -85,6 +135,7 @@ export default {
   }
   .nav-header {
     .container {
+      position: relative;
       height: 112px;
       @include flex();
       .header-logo {
@@ -124,6 +175,67 @@ export default {
           margin-right: 20px;
           span {
             cursor: pointer;
+          }
+          &:hover {
+            color: $colorA;
+            .children {
+              height: 220px;
+              opacity: 1;
+            }
+          }
+          .children {
+            position: absolute;
+            top: 112px;
+            left: 0;
+            width: 1226px;
+            height: 0;
+            opacity: 0;
+            overflow: hidden;
+            border-top: 1px solid #e5e5e5;
+            box-shadow: 0px 7px 6px 0px rgba($color: #000000, $alpha: 0.11);
+            z-index: 10;
+            transition: all 0.5s;
+            .product {
+              position: relative;
+              float: left;
+              width: 16.66%;
+              height: 220px;
+              font-size: 12px;
+              line-height: 12px;
+              text-align: center;
+              a {
+                display: inline-block;
+              }
+              img {
+                width: auto;
+                height: 110px;
+                margin-top: 26px;
+              }
+              .pro-img {
+                height: 137px;
+              }
+              .pro-name {
+                font-weight: bold;
+                margin-top: 20px;
+                margin-bottom: 8px;
+                color: $colorB;
+              }
+              .pro-price {
+                color: $colorA;
+              }
+              &:before {
+                content: "";
+                position: absolute;
+                top: 28px;
+                right: 0;
+                border-left: 1px solid $colorF;
+                height: 100px;
+                width: 1px;
+              }
+              &:last-child:before {
+                display: none;
+              }
+            }
           }
         }
       }
