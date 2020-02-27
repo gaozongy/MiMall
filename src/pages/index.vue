0757c74fe7aba5e9eadf2ebe7df66b76d72d3ede
GaozyDev <670453367@qq.com>
2020-02-27 16:46:56 +0800

    首页轮播实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,35 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+            </li>
+          </ul>
+        </div>
         <swiper v-bind:options="swiperOption">
           <swiper-slide v-for="(item, index) in slideList" v-bind:key="index">
             <a v-bind:href="'/#/product/' + item.id">
@@ -34,18 +63,20 @@ export default {
   data() {
     return {
       swiperOption: {
-        autoplay: true,
+        autoplay: {
+          delay: 8000
+        },
         loop: true,
-        effect: "cube",
-        cubeEffect: {
-          shadowOffset: 100,
-          shadowScale: 0.6
+        speed: 1500,
+        effect: "fade",
+        fadeEffect: {
+          crossFade: true
         },
         pagination: {
           el: ".swiper-pagination",
           clickable: true
         },
-        natigation: {
+        navigation: {
           prevEl: ".swiper-button-prev",
           nextEl: ".swiper-button-next"
         }
@@ -69,7 +100,7 @@ export default {
         },
         {
           id: "",
-          img: "/imgs/slider/slide-1.jpg"
+          img: "/imgs/slider/slide-5.jpg"
         }
       ]
     };
@@ -78,10 +109,79 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
 .index {
   .swiper-box {
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 450px;
+      z-index: 9;
+      padding: 26px 0;
+      background-color: #55585a7a;
+      box-sizing: border-box;
+      .menu-item {
+        height: 50px;
+        line-height: 50px;
+        a {
+          position: relative;
+          display: block;
+          font-size: 16px;
+          color: #ffffff;
+          padding-left: 30px;
+          &:after {
+            position: absolute;
+            right: 30px;
+            top: 17.5px;
+            content: " ";
+            @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
+          }
+        }
+        &:hover {
+          background-color: $colorA;
+          .children {
+            display: block;
+          }
+        }
+        .children {
+          display: none;
+          width: 962px;
+          height: 450px;
+          background-color: $colorG;
+          position: absolute;
+          top: 0;
+          left: 264px;
+          border: 1px solid $colorH;
+          ul {
+            display: flex;
+            justify-content: space-between;
+            height: 75px;
+            li {
+              height: 75px;
+              line-height: 75px;
+              flex: 1;
+              padding-left: 22px;
+            }
+            a {
+              color: $colorB;
+              font-size: 14px;
+            }
+            img {
+              width: 42px;
+              height: 35px;
+              vertical-align: middle;
+              margin-right: 15px;
+            }
+          }
+        }
+      }
+    }
     .swiper-container {
       height: 450px;
+      .swiper-button-prev {
+        left: 274px;
+      }
       img {
         width: 100%;
         height: 100%;
