801e46130c5e62b1fd040fa4f826e3396fb33009
GaozyDev <670453367@qq.com>
2020-02-28 10:55:41 +0800

    首页广告位

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -51,8 +51,16 @@
           <div class="swiper-button-next" slot="button-next" />
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a v-bind:href="'/#/product/' + item.id" v-for="(item, index) in adsList" v-bind:key="index">
+          <img v-bind:src="item.img"/>
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="imgs/banner-1.png"/>
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar />
@@ -135,7 +143,22 @@ export default {
             name: "移动4G专区"
           }
         ]
-      ]
+      ],
+      adsList:[
+          {
+            id:33,
+            img:'/imgs/ads/ads-1.png'
+          },{
+            id:48,
+            img:'/imgs/ads/ads-2.jpg'
+          },{
+            id:45,
+            img:'/imgs/ads/ads-3.png'
+          },{
+            id:47,
+            img:'/imgs/ads/ads-4.jpg'
+          }
+        ]
     };
   }
 };
@@ -230,6 +253,18 @@ export default {
         height: 100%;
       }
     }
+  }
+  .ads-box {
+    @include flex();
+    margin-top:14px;
+    margin-bottom:30px;
+    a {
+      width: 296px;
+      height: 168px;
+    }
+  }
+  .banner {
+    margin-bottom:50px;
   }
 }
 </style>
